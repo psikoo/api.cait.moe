1332dd239ddf2fa831b70d171412e1ffc05ba0a3
psikoo <hugohernandezmuelas03@gmail.com>
2025-10-01 09:32:37 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,9 +16,11 @@ import { UrlsModule } from './modules/y2k/urls/urls.module';
 import { UsersModule } from './modules/y2k/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, 
-            TamagotchiModule, ShortUrlModule, 
-            CamerasModule, PhotosModule, ApisModule, CounterModule, ProjectsModule, SongsModule, UrlsModule, UsersModule
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), DatabaseModule,             // Config
+    TamagotchiModule, ShortUrlModule, ApisModule,                         // Other
+    CamerasModule, PhotosModule,                                          // DGT
+    CounterModule, ProjectsModule, SongsModule, UrlsModule, UsersModule   // Y2K
   ],
   controllers: [AppController],
   providers: [AppService],
