51d859b5082bd694cb86f5656ee5e0347258dc7b
psikoo <hugohernandezmuelas03@gmail.com>
2025-10-01 09:41:10 +0200

    Update counter.entity.ts

--- src/modules/y2k/counter/entities/counter.entity.ts
+++ src/modules/y2k/counter/entities/counter.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
-export class Counter {
+export class Y2kCounter {
   @PrimaryColumn({ default:0 })
   id: number;
   @Column({nullable:false, default:0 })
