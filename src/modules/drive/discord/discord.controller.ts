c39aae648b6f0e8f9ad79765dff4203e1a92a145
psikoo <91342186+psikoo@users.noreply.github.com>
2025-12-19 22:28:05 +0100

    Update discord.controller.ts

--- src/modules/drive/discord/discord.controller.ts
+++ src/modules/drive/discord/discord.controller.ts
@@ -14,7 +14,9 @@ export class DiscordController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  postUrl(@Query() query: Discord, @UploadedFile() file: any): Promise<JSON> {
+  postUrl(@Query() query: Discord, @Body() body: any, @UploadedFile() file: any): Promise<JSON> {
+    console.log(body)
+    console.log(file)
     return this.discordService.postUrl(query, file);
   }
 }
