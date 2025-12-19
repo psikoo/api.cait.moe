237e8a4c5fae89fde37b6f826049637174e15f9e
psikoo <91342186+psikoo@users.noreply.github.com>
2025-12-19 22:55:14 +0100

    a

--- src/modules/drive/discord/discord.service.ts
+++ src/modules/drive/discord/discord.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import FormData from 'form-data';
 import { Discord } from './dto';
 
 @Injectable()
@@ -20,16 +21,9 @@ export class DiscordService {
   async postUrl(query: Discord, file: any): Promise<JSON> {
     let finalResponse = "empty";
     const form = new FormData();
-    const fileBlob = new Blob([file.buffer]);
-    form.append("file", fileBlob, file.originalname || "file");
-    const payload = JSON.stringify({ content: "File", attachments: [{ id: 0, description: "Uploaded via api" }] });
-    form.append("payload_json", payload);
-    let config = {
-      method: "post",
-      url: "https://discord.com"+query.path,
-      headers: { "Authorization": "Bot "+query.token }
-    };
-    await axios.request(config) 
+    form.append("file", file.buffer, { filename: file.originalname || "file", contentType: file.mimetype });
+    form.append('payload_json', JSON.stringify({ content: '' }));
+    await axios.post("https://discord.com"+query.path, form, {headers: { ...form.getHeaders(), "Authorization": "Bot "+query.token }}) 
     .then((response) => { finalResponse = response.data; })
     .catch((error) => { finalResponse = error; });
     return JSON.parse(JSON.stringify(finalResponse));
